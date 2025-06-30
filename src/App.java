4cae66a24925dcdc3dc1b61aa8d9679b743f9fdd
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-06-30 17:14:43 -0300

    :white_check_mark: Debug entendo o ponteiro

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
+import AlgoritmosOrdenacao.BubbleSort;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int vetor[] = {5, 3, 8, 2};
+
+        System.out.println(Arrays.toString(vetor));
+        BubbleSort.algoritmo(vetor);
+        System.out.println(Arrays.toString(vetor));
     }
 }
